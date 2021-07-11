e49e1966ecf8999f08327aa62f46ed8ba64689e4
cliffybar <crowellclifford@utexas.edu>
2021-07-11 18:09:12 -0500

    finished app

---
+++ public/js/home.js
@@ -0,0 +1,21 @@
+const inactivityTime = function () {
+    const time;
+
+    window.onload = resetTimer;
+
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+
+    function logoutMeOut() {
+        logout();
+    }
+
+    function resetTimer() {
+        clearTimeout(time);
+        time = setTimeout(logoutMeOut, 600000);
+    }
+};
+
+window.onload = function () {
+    inactivityTime();
+};
